99833dc4a0fc0f838eae459a78e42ca038b4b569
MeeXaSiK <87555873+MeeXaSiK@users.noreply.github.com>
2022-12-02 02:50:45 +0700

    Update StateMachineUser.cs

--- Code/NightStateMachine/StateMachineUser.cs
+++ Code/NightStateMachine/StateMachineUser.cs
@@ -16,11 +16,13 @@ namespace NTC.Global.StateMachine
             NightStateMachine.On<RunningState>(OnGameRun, gameObject);
             NightStateMachine.On<WinState>(OnGameWin, gameObject);
             NightStateMachine.On<LoseState>(OnGameLose, gameObject);
+            NightStateMachine.On<WinState, LoseState>(OnGameFinish, gameObject);
         }
         
         protected virtual void OnAwake() { }
         protected virtual void OnGameRun() { }
         protected virtual void OnGameWin() { }
         protected virtual void OnGameLose() { }
+        protected virtual void OnGameFinish() { }
     }
 }
